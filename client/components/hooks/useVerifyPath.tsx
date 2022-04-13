ced37c5ac8638b3c3e7209d0225391425bb0521d
disco07 <koneenok@outlook.fr>
2022-04-13 08:48:22 +0200

    remove cache

--- client/components/hooks/useVerifyPath.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const useVerifyPath = (): boolean => {
-  const router = useRouter();
-  const route = ['search-offers', 'search-places', 'search'];
-  return route.includes(router.route.split('/')[1]);
-};
-
-export default useVerifyPath;
--- client/pages/mon-compte.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Login from '../components/Login';
-
-const MonCompte = (): JSX.Element => {
-  return <Login />;
-};
-
-export default MonCompte;
